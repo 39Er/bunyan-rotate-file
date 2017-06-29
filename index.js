91d0cf90d312bd73421adfb4dc41df463738a546
39Er <liuweiminfirst@163.com>
2017-06-29 18:31:46 +0800

    modify maxSize

--- index.js
+++ index.js
@@ -16,18 +16,23 @@ const safeCycles = bunyan.safeCycles;
 module.exports = class RotateFileStream {
 
   constructor(logpath, maxSize) {
+    if (arguments.length === 2) {
+      this.maxSize = maxSize;
+    }
     this.logpath = logpath;
-    this.maxSize = maxSize;
     this.logger = null;
   }
 
   write(rec) {
-    this.logger = _createStream(this.logpath, this.logger, this.maxSize);
+    if (this.maxSize === undefined) {
+      this.logger = _createStream(this.logpath, this.logger);
+    } else {
+      this.logger = _createStream(this.logpath, this.logger, this.maxSize);
+    }
     rec.time = moment().format('YYYY-MM-DDTHH:mm:ss');
     let str = JSON.stringify(rec, safeCycles()) + '\n';
     this.logger.write(str);
   }
-
 }
 
 /**
@@ -35,25 +40,27 @@ module.exports = class RotateFileStream {
  * @param {String} logpath 
  */
 function _createStream(logpath, logger, maxSize) {
-  if (isNaN(maxSize) || maxSize < 1) {
+  if (arguments.length === 3 && (isNaN(maxSize) || maxSize < 1)) {
     throw new Error('maxSize should be a number and bigger than 1');
   }
   try {
     let stat = fs.statSync(logpath);
-    if (moment(stat.atime, 'YYYYMMDD').diff(moment().format('YYYYMMDD'), 'days') !== 0) {
+    if (moment(stat.atime, 'YYYYMMDD').diff(moment(), 'days') !== 0) {
       if (logger) {
         logger.close();
         logger = null;
       }
-      fs.renameSync(logpath, util.format('%s.%s', logpath, moment(stat.ctime).format('YYYYMMDD')));
+      fs.renameSync(logpath, util.format('%s.%s', logpath, moment(stat.atime).format('YYYYMMDD')));
       let logdir = logpath.slice(0, logpath.lastIndexOf(sep));
       let files = fs.readdirSync(logdir);
       let currentNum = files.length;
-      let i = 1;
-      while (maxSize < currentNum) {
-        fs.unlinkSync(path.join(logdir, files[i]));
-        i += 1;
-        currentNum -= 1;
+      if (arguments.length === 3) {
+        let i = 0;
+        while (maxSize <= currentNum) {
+          fs.unlinkSync(path.join(logdir, files[i]));
+          i += 1;
+          currentNum -= 1;
+        }
       }
     }
   } catch (e) {
